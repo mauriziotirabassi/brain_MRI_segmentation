b841c7aa126cb3955a981e7d889b2244bf758441
Maurizio Tirabassi <maurizio.tirabassi@mail.polimi.it>
2023-11-30 14:28:20 +0100

    brian

--- src/MI_Team17Topic03_Code.m
+++ src/MI_Team17Topic03_Code.m
@@ -7,3 +7,41 @@ if isunix
 else
     path = '';
 end
+
+load('MRIdata.mat');
+imshow(imread('memref.jpg'))
+
+%% DATA PREPROCESSING
+% Removal of singleton dimensions
+brian = squeeze(vol);
+
+%% VOLUME VISUALIZATION
+% https://www.mathworks.com/help/images/exploring-slices-from-a-3-dimensional-mri-data-set.html
+
+% Displaying the MRI volume across the three planes
+figure
+orthosliceViewer(brian)
+
+%% 
+% Displaying single planes
+figure, sliceViewer(brian, SliceDirection="X"), title("Coronal Plane")
+figure, sliceViewer(brian, SliceDirection="Y"), title("Sagittal Plane")
+figure, sliceViewer(brian, SliceDirection="Z"), title("Axial Plane")
+
+%% 
+% Slice planes
+sx = 1; sy = 1; sz = 2.5;
+A = [sx 0 0 0; 0 sy 0 0; 0 0 sz 0; 0 0 0 1];
+tform = affinetform3d(A);
+brian2 = volshow(brian,renderingStyle="SlicePlanes",Transformation=tform);
+
+%%
+% Volume rendering
+volshow(brian, RenderingStyle="VolumeRendering")
+
+%%
+% Volume viewer
+volumeViewer(brian)
+
+
+
